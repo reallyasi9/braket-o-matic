0984e2eb54f06b068285f19b5d35e2d074c2eb20
Phil Killewald <phillip.killewald@gmail.com>
2014-12-26 16:16:18 -0500

    Attempting to get joining and splitting working

--- src/net/exclaimindustries/paste/braket/client/ui/TournamentInfoPanel.java
+++ src/net/exclaimindustries/paste/braket/client/ui/TournamentInfoPanel.java
@@ -3,6 +3,7 @@ package net.exclaimindustries.paste.braket.client.ui;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
 import net.exclaimindustries.paste.braket.client.BraketTournament;
 import net.exclaimindustries.paste.braket.client.TournamentService;
 import net.exclaimindustries.paste.braket.client.TournamentService.TournamentCollection;
@@ -14,11 +15,13 @@ import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.i18n.client.DateTimeFormat;
+import com.google.gwt.thirdparty.guava.common.base.Joiner;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.DoubleBox;
 import com.google.gwt.user.client.ui.Label;
@@ -43,15 +46,21 @@ public class TournamentInfoPanel extends Composite {
 
   @UiField
   TextBox nameBox;
-  
+
   @UiField
   Label nameStatus;
 
   @UiField
   DateBox startDateBox;
 
+  @UiField
+  Label startDateStatus;
+
   @UiField(provided = true)
   HourMinutePicker startTimeBox;
+
+  @UiField
+  Label startTimeStatus;
 
   @UiField
   Label currentLabel;
@@ -59,8 +68,20 @@ public class TournamentInfoPanel extends Composite {
   @UiField
   DoubleBox buyInBox;
 
-  // @UiField
-  // TextBox payOutBox;
+  @UiField
+  Label buyInStatus;
+
+  @UiField
+  TextBox payOutValuesBox;
+
+  @UiField
+  CheckBox payOutValuesRemainderCheckBox;
+
+  @UiField
+  Label payOutValuesRemainderLabel;
+
+  @UiField
+  Label payOutValuesStatus;
 
   @UiField
   DoubleBox upsetBox;
@@ -72,6 +93,9 @@ public class TournamentInfoPanel extends Composite {
   Button updateButton;
 
   @UiField
+  Button deleteButton;
+
+  @UiField
   Button selectButton;
 
   // Members ------------------------------------------------------------------
@@ -81,7 +105,7 @@ public class TournamentInfoPanel extends Composite {
   private BraketTournament currentTournament;
 
   private AsyncDataProvider<BraketTournament> dataProvider;
-  
+
   /**
    * Service for reading and editing Tournament data
    */
@@ -89,113 +113,6 @@ public class TournamentInfoPanel extends Composite {
       .create(TournamentService.class);
 
   // Callbacks ----------------------------------------------------------------
-
-  private AsyncCallback<Long> storeTournamentCallback = new AsyncCallback<Long>() {
-
-    @Override
-    public void onFailure(Throwable caught) {
-      setSelectedTournament(tournament);
-      updateButton.setEnabled(tournament != null);
-      createButton.setEnabled(true);
-      selectButton.setEnabled(tournament != currentTournament);
-    }
-
-    @Override
-    public void onSuccess(Long result) {
-      // TODO add to list
-      tournament.setId(result);
-      dataProvider.getKey(tournament);
-      List<BraketTournament> tournaments = new ArrayList<BraketTournament>();
-      tournaments.add(tournament);
-      dataProvider.updateRowData(Integer.MAX_VALUE, tournaments);
-      setSelectedTournament(tournament);
-      selectButton.setEnabled(true);
-    }
-  };
-
-  private AsyncCallback<BraketTournament> updateCurrentTournamentCallback = new AsyncCallback<BraketTournament>() {
-    @Override
-    public void onFailure(Throwable caught) {
-      // TODO Auto-generated method stub
-      selectButton.setEnabled(currentTournament != tournament);
-      updateButton.setEnabled(true);
-      createButton.setEnabled(true);
-    }
-
-    @Override
-    public void onSuccess(BraketTournament result) {
-      // TODO add a badge
-      selectButton.setEnabled(false);
-      currentTournament = result;
-      setSelectedTournament(currentTournament);
-    }
-
-  };
-
-  private ClickHandler createTournamentHandler = new ClickHandler() {
-    @Override
-    public void onClick(ClickEvent event) {
-
-      updateButton.setEnabled(false);
-      createButton.setEnabled(false);
-      selectButton.setEnabled(false);
-
-      tournament = new BraketTournament();
-      updateTournamentFromInput();
-
-      // Validate first
-      // TODO use a real validation engine
-      nameStatus.setText("");
-      boolean invalid = false;
-      if (tournament.getName().isEmpty()) {
-        nameStatus.setText("Tournament name cannot be empty");
-        invalid = true;
-      }
-      
-      if (!invalid) {
-        tournamentService.storeTournament(tournament, storeTournamentCallback);
-      } else {
-        updateButton.setEnabled(true);
-        createButton.setEnabled(true);
-        selectButton.setEnabled(true);
-      }
-    }
-  };
-
-  private ClickHandler updateTournamentHandler = new ClickHandler() {
-    @Override
-    public void onClick(ClickEvent event) {
-      if (tournament == null) {
-        return;
-      }
-
-      updateButton.setEnabled(false);
-      createButton.setEnabled(false);
-      selectButton.setEnabled(false);
-      updateTournamentFromInput();
-
-      // TODO more stuff here (like validation?)
-
-      tournamentService.storeTournament(tournament, storeTournamentCallback);
-    }
-  };
-
-  private ClickHandler selectCurrentTournamentHandler = new ClickHandler() {
-
-    @Override
-    public void onClick(ClickEvent event) {
-      if (tournament == null) {
-        return;
-      }
-      if (!Window.confirm("TODO: Yes/No Update?")) {
-        return;
-      }
-      selectButton.setEnabled(false);
-      tournamentService.setCurrentTournament(tournament,
-          updateCurrentTournamentCallback);
-    }
-
-  };
 
   private AsyncCallback<TournamentCollection> getCurrentTournamentCallback = new AsyncCallback<TournamentCollection>() {
     @Override
@@ -213,6 +130,116 @@ public class TournamentInfoPanel extends Composite {
     }
   };
 
+  private AsyncCallback<Long> storeTournamentCallback = new AsyncCallback<Long>() {
+
+    @Override
+    public void onFailure(Throwable caught) {
+      setSelectedTournament(tournament);
+    }
+
+    @Override
+    public void onSuccess(Long result) {
+      // TODO add to list
+      tournament.setId(result);
+      dataProvider.getKey(tournament);
+      List<BraketTournament> tournaments = new ArrayList<BraketTournament>();
+      tournaments.add(tournament);
+      dataProvider.updateRowData(Integer.MAX_VALUE, tournaments);
+      setSelectedTournament(tournament);
+    }
+  };
+
+  private AsyncCallback<BraketTournament> updateCurrentTournamentCallback = new AsyncCallback<BraketTournament>() {
+    @Override
+    public void onFailure(Throwable caught) {
+      // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onSuccess(BraketTournament result) {
+      // TODO add a badge
+      currentTournament = result;
+      setSelectedTournament(currentTournament);
+    }
+
+  };
+
+  private AsyncCallback<Void> deleteTournamentCallback = new AsyncCallback<Void>() {
+    @Override
+    public void onFailure(Throwable caught) {
+      // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onSuccess(Void result) {
+      // TODO put badge on list?
+      if (tournament == currentTournament) {
+        currentTournament = null;
+      }
+      setSelectedTournament(null);
+
+    }
+  };
+
+  private ClickHandler createTournamentHandler = new ClickHandler() {
+    @Override
+    public void onClick(ClickEvent event) {
+      tournament = new BraketTournament();
+      if (validateFields()) {
+        updateTournamentFromInput();
+        tournamentService.storeTournament(tournament, storeTournamentCallback);
+      }
+    }
+  };
+
+  private ClickHandler updateTournamentHandler = new ClickHandler() {
+    @Override
+    public void onClick(ClickEvent event) {
+      if (tournament == null) {
+        return;
+      }
+      updateTournamentFromInput();
+
+      // TODO more stuff here (like validation?)
+
+      tournamentService.storeTournament(tournament, storeTournamentCallback);
+    }
+  };
+
+  private ClickHandler selectCurrentTournamentHandler = new ClickHandler() {
+
+    @Override
+    public void onClick(ClickEvent event) {
+      if (tournament == null) {
+        return;
+      }
+      // TODO
+      if (!Window.confirm("TODO: Yes/No Update?")) {
+        return;
+      }
+      tournamentService.setCurrentTournament(tournament,
+          updateCurrentTournamentCallback);
+    }
+
+  };
+
+  private ClickHandler deleteTournamentHandler = new ClickHandler() {
+
+    @Override
+    public void onClick(ClickEvent event) {
+      if (tournament == null) {
+        return;
+      }
+      // TODO
+      if (!Window.confirm("TODO: Yes/No Delete?")) {
+        return;
+      }
+      tournamentService.deleteTournament(tournament, deleteTournamentCallback);
+    }
+
+  };
+
   private ChangeHandler changeHandler = new ChangeHandler() {
 
     @Override
@@ -221,6 +248,16 @@ public class TournamentInfoPanel extends Composite {
         updateButton.setEnabled(true);
       }
       createButton.setEnabled(true);
+    }
+
+  };
+
+  private ClickHandler payOutValuesRemainderHandler = new ClickHandler() {
+
+    @Override
+    public void onClick(ClickEvent event) {
+      payOutValuesRemainderCheckBox.setValue(
+          !payOutValuesRemainderCheckBox.getValue(), true);
     }
 
   };
@@ -234,36 +271,69 @@ public class TournamentInfoPanel extends Composite {
    */
   public TournamentInfoPanel(AsyncDataProvider<BraketTournament> dp) {
     dataProvider = dp;
-    
+
     startTimeBox = new HourMinutePicker(HourMinutePicker.PickerFormat._24_HOUR);
-    initWidget(uiBinder.createAndBindUi(this));
 
     DateTimeFormat dateFormat = DateTimeFormat.getFormat("yyyy.MM.dd");
     startDateBox.setFormat(new DateBox.DefaultFormat(dateFormat));
 
+    // Add a bunch of change handlers
     nameBox.addChangeHandler(changeHandler);
     startDateBox.getTextBox().addChangeHandler(changeHandler);
     startTimeBox.addHandler(changeHandler, ChangeEvent.getType());
     buyInBox.addChangeHandler(changeHandler);
     upsetBox.addChangeHandler(changeHandler);
+    payOutValuesRemainderCheckBox.addHandler(changeHandler,
+        ChangeEvent.getType());
 
     // Initialize the tournament to null.
     setSelectedTournament(null);
 
-    // Define buttons
+    // Define buttons and click handlers
     createButton.addClickHandler(createTournamentHandler);
-
     updateButton.addClickHandler(updateTournamentHandler);
-
     selectButton.addClickHandler(selectCurrentTournamentHandler);
+    deleteButton.addClickHandler(deleteTournamentHandler);
+    payOutValuesRemainderLabel.addClickHandler(payOutValuesRemainderHandler);
 
     // Lookup the current tournament
     tournamentService.getCurrentTournament(getCurrentTournamentCallback);
+
+    // Initialize the UI
+    initWidget(uiBinder.createAndBindUi(this));
+  }
+
+  protected boolean validateFields() {
+    // Validate first
+    // TODO use a real validation engine
+    nameStatus.setText("");
+    startDateStatus.setText("");
+    startTimeStatus.setText("");
+    buyInStatus.setText("");
+    boolean invalid = false;
+    if (nameBox.getText().isEmpty()) {
+      nameStatus.setText("Tournament name cannot be empty");
+      invalid = true;
+    }
+    if (startDateBox.getValue() == null) {
+      startDateStatus.setText("Start date must be in the format "
+          + startDateBox.getFormat().toString());
+      invalid = true;
+    }
+    if (startTimeBox.getMinutes() == null) {
+      startTimeStatus.setText("Start time must be in the format HH:mm");
+      invalid = true;
+    }
+    if (buyInBox.getValue() == null) {
+      buyInStatus.setText("Buy-in must be a number");
+      invalid = true;
+    }
+
+    return (!invalid);
   }
 
   public void setSelectedTournament(BraketTournament tourn) {
     this.tournament = tourn;
-    updateButton.setEnabled(tourn != null);
     updateInputFromTournament();
   }
 
@@ -282,6 +352,15 @@ public class TournamentInfoPanel extends Composite {
 
       Double upsetValue = Double.valueOf(upsetBox.getValue());
       tournament.setUpsetValue(upsetValue);
+
+      List<Double> payOuts = new ArrayList<Double>();
+      for (String strVal : payOutValuesBox.getValue().split(",[ ]*")) {
+        payOuts.add(Double.valueOf(strVal));
+      }
+      if (payOutValuesRemainderCheckBox.getValue()) {
+        payOuts.add(0, null);
+      }
+      tournament.setPayOutValues(payOuts);
     }
   }
 
@@ -302,6 +381,16 @@ public class TournamentInfoPanel extends Composite {
       buyInBox.setValue(tournament.getBuyInValue());
 
       upsetBox.setValue(tournament.getUpsetValue());
+
+      List<Double> payOuts = tournament.getPayOutValues();
+      if (payOuts.get(0) == null) {
+        payOutValuesRemainderCheckBox.setValue(true);
+        payOutValuesBox.setValue(Joiner.on(", ").join(
+            payOuts.subList(1, payOuts.size())));
+      } else {
+        payOutValuesRemainderCheckBox.setValue(false);
+        payOutValuesBox.setValue(Joiner.on(", ").join(payOuts));
+      }
 
       if (currentTournament != null) {
         currentLabel
