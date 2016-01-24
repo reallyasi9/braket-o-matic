6b34df47f2c6f225b8c1851dc9611ba34b58da5c
Phil Killewald <phillip.killewald@gmail.com>
2016-01-23 20:42:16 -0500

    Adding calculation of potential selection value.

--- backend/selection.go
+++ backend/selection.go
@@ -48,83 +48,102 @@ func contract(a int64, n uint64) int64 {
 	return out
 }
 
-func tally(sel int64, tru int64) float32 {
+// Score a given selection (sel) against a tournament outcome (tru) given
+// certain games have been played already (played).
+// This function assumes a 6-round (64-team) tournament, and the existance of a
+// global 6-element vector roundValues containing the number of points per
+// tournament round.
+func tally(sel int64, tru int64, played int64) (float32, float32) {
 	a := sel
 	b := tru
+	p := played
 
 	// -----first round-----
+	roundMask := int64(0x00000000ffffffff)
 
-	// Nothing besides whether or not the selections match matter here.
-	correct := ^(a ^ b)
-
-	// Only look at the first round games
-	correct &= 0xffffffff
+	// In the first round, just check if the selection matches the outcome.
+	correct := ^(a ^ b) & roundMask
+	maybe := ^p & roundMask
 
 	//fmt.Printf("round 1\n")
-	//fmt.Printf("%032b a\n", a)
-	//fmt.Printf("%032b b\n", b)
+	//fmt.Printf("%064b a\n", a)
+	//fmt.Printf("%064b b\n", b)
 
-	// Count the number correct
-	score := float32(popcount(correct)) * roundValues[0]
+	// Count the number correct.  Only count the games played
+	score := float32(popcount(correct&p)) * roundValues[0]
 
-	//fmt.Printf("%032b correct * %f = %f\n", correct, roundValues[0], score)
+	// Count the number yet to be played
+	potential := float32(popcount(maybe)) * roundValues[0]
+
+	//fmt.Printf("%064b correct * %f = %f\n", correct, roundValues[0], score)
+
+	// At this point, assume correct all those games that have not yet been played
+	correct |= ^p
 
 	// Move on to the next round
 	a >>= nGames[0]
 	b >>= nGames[0]
+	p >>= nGames[0]
 
 	// -----all other rounds-----
 	for rnd := uint64(1); rnd < nRounds; rnd++ {
 		// Because we shifted the selection over, the lowest nGames bits are all we want.
 		// nGames follows the pattern 2^(6-rnd) = 1 << (nRounds - rnd)
-		roundMask := int64((1 << (1 << (nRounds - rnd - 1))) - 1)
+		roundMask = int64((1 << (1 << (nRounds - rnd - 1))) - 1)
 
-		//fmt.Printf("round %d, mask %032b\n", rnd+1, roundMask)
+		//fmt.Printf("round %d, mask %064b\n", rnd+1, roundMask)
 
 		// This trick expands a set of bits, so 0bWXYZ -> 0b0W0X0Y0Z
 		c := expand(a, nGames[rnd])
 
-		//fmt.Printf("%032b expanded from %032b\n", c, a&roundMask)
+		//fmt.Printf("%064b expanded from\n%064b\n", c, a&roundMask)
 
 		/* In order to determine which game in the past round produced who we picked
 		   as the winner in this round, we have to convert a 0b0 in this round to a
 		   0b01 in the previous round, and a 0b1 to a 0b10.  That's the same as
 		   adding 1 to every 2-bit pair, or adding 0x5555..... */
-		c += 0x5555555555555555
+		c += m1
 
-		//fmt.Printf("%032b c + odds\n", c)
-		//fmt.Printf("%032b correct (last round)\n", correct)
+		//fmt.Printf("%064b c + odds\n", c)
+		//fmt.Printf("%064b correct (last round)\n", correct)
 
 		// Given the bits we care about last round, select out the ones we got right.
 		forward := c & correct
 
-		//fmt.Printf("%032b forward mask\n", forward)
+		//fmt.Printf("%064b forward mask\n", forward)
 
 		/* Now contract the mask, effectively ORing every two bits.  So if we picked
 		   correctly at least the game in the last round that matches the selection
 		   we chose this round, we can consider it for points this round. */
 		forwardMask := contract(forward, nGames[rnd])
 
-		//fmt.Printf("%032b contracted\n", forwardMask)
-		//fmt.Printf("%032b a\n", a)
-		//fmt.Printf("%032b b\n", b)
+		//fmt.Printf("%064b contracted\n", forwardMask)
+		//fmt.Printf("%064b a\n", a)
+		//fmt.Printf("%064b b\n", b)
 
 		/* Only select as correct those picks that match truth, are in the round we
 		   are currently considering, and that we got right before (so the correct
 		   team is propagated to this game). */
 		correct = ^(a ^ b) & roundMask & forwardMask
+		maybe = ^p & roundMask & forwardMask
 
-		//fmt.Printf("%032b correct * %f = %f\n", correct, roundValues[rnd], float32(popcount(correct))*roundValues[rnd])
+		//fmt.Printf("%064b correct * %f = %f\n", correct, roundValues[rnd], float32(popcount(correct))*roundValues[rnd])
 
 		// Count the number correct
-		score += float32(popcount(correct)) * roundValues[rnd]
+		score += float32(popcount(correct&p)) * roundValues[rnd]
+		potential += float32(popcount(maybe)) * roundValues[rnd]
+
+		// At this point, assume correct all those games that have not yet been played,
+		// but also are not ruled out by getting previous picks wrong.
+		correct |= ^p & forwardMask
 
 		// Move on to the next round
 		a >>= nGames[rnd]
 		b >>= nGames[rnd]
+		p >>= nGames[rnd]
 	}
 
-	return score
+	return score, potential
 }
 
 // See https://en.wikipedia.org/wiki/Hamming_weight
