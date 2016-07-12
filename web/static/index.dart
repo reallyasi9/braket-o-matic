443d0eba36514c2ce0133dcb36f5ff2922319405
Phil Killewald <phillip.killewald@gmail.com>
2016-07-11 21:44:29 -0400

    Adding tab selection.

--- web/static/index.dart
+++ web/static/index.dart
@@ -5,6 +5,12 @@ import 'lib/user.dart';
 
 main() async {
     await initPolymer();
+
+    // Bind tab selector
+    Element tabs = document.querySelector("paper-tabs");
+    Element pages = document.querySelector("iron-pages");
+    tabs.addEventListener("iron-select", onTabSelect);
+
     try {
         HttpRequest.getString("/backend/get-user").then(onUserLoaded);
     } catch (e) {
@@ -16,4 +22,10 @@ onUserLoaded(String jsonUser) async {
     // I seriously can't believe this just works.
     User user = new UserImpl.fromJsonString(jsonUser);
     print(user.AppEngineUser.Email);
+}
+
+onTabSelect(Event event) async {
+    Element tabs = document.querySelector("paper-tabs");
+    Element pages = document.querySelector("iron-pages");
+    pages.selected = tabs.selected;
 }
