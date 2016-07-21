2709242fd5591e0ae61e3f5463b2f3fe8f0cb773
Phil Killewald <phillip.killewald@gmail.com>
2016-07-20 20:20:26 -0400

    Adding file upload element (still JS for now).

---
+++ web/static/elements/file_upload.dart
@@ -0,0 +1,6 @@
+/*
+@license
+Original code Copyright (c) 2015 Winston Howes. All rights reserved.
+This code may only be used under the MIT license found at https://github.com/winhowes/file-upload/blob/master/LICENSE
+Modified for use with Polymer Dart by Phil Killewald (c) 2016.
+*/
