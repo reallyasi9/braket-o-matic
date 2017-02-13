c91d0a5c84280b1ef444c6bd76e38d9b9e016b0e
Phil Killewald <phillip.killewald@gmail.com>
2017-02-12 22:50:27 -0500

    Adding potential way to deal with displaying games.

---
+++ web/static/elements/round_scroller.dart
@@ -0,0 +1,24 @@
+@HtmlImport('round_scroller.html')
+library braket.round_scroller;
+
+import 'package:polymer/polymer.dart';
+import 'package:web_components/web_components.dart' show HtmlImport;
+
+import '../lib/game.dart';
+
+@PolymerRegister('round-scroller')
+class RoundScroller extends PolymerElement {
+  RoundScroller.created() : super.created();
+
+  // Collection of games to display, sorted by round/game (hopefully)
+  @property
+  List<Game> games = [];
+
+  @property
+  int currentRound = 0;
+
+  // Return the games in a given round, in round order.  Rounds begin at 0.
+  @reflectable
+  bool inCurrentRound(Game g) => g.round == currentRound;
+
+}
