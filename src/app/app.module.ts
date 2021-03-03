5daedc5ed33e62349d735a8b5cb6200126f3b3f6
Phil Killewald <reallyasi9@users.noreply.github.com>
2021-03-03 14:19:06 -0500

    Remove unused code and imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { USE_EMULATOR as USE_AUTH_EMULATOR } from '@angular/fire/auth';
+import { USE_EMULATOR as USE_FIRESTORE_EMULATOR } from '@angular/fire/firestore';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxAuthFirebaseUIModule } from 'ngx-auth-firebaseui';
 import { environment } from 'src/environments/environment';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog';
 
 import { AppComponent } from './app.component';
 import { UserButtonComponent } from './user-button/user-button.component';
@@ -14,18 +14,6 @@ import { UserLoginComponent } from './user-login/user-login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
 import { UserRegisterComponent } from './user-register/user-register.component';
-
-// const firebaseUiAuthConfig: firebaseui.auth.Config = {
-//   signInFlow: 'popup',
-//   signInOptions: [
-//     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-//     firebase.auth.EmailAuthProvider.PROVIDER_ID,
-//     firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID,
-//   ],
-//   // tosUrl: '<your-tos-link>',
-//   // privacyPolicyUrl: '<your-privacyPolicyUrl-link>',
-//   credentialHelper: firebaseui.auth.CredentialHelper.GOOGLE_YOLO,
-// };
 
 const routes: Routes = [
   { path: 'login', component: UserLoginComponent },
@@ -51,6 +39,10 @@ const routes: Routes = [
     {
       provide: USE_AUTH_EMULATOR,
       useValue: !environment.production ? ['localhost', 9099] : undefined,
+    },
+    {
+      provide: USE_FIRESTORE_EMULATOR,
+      useValue: !environment.production ? ['localhost', 8080] : undefined,
     },
   ],
   bootstrap: [AppComponent],
