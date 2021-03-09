14c07cb091e736ecd0ac65e6dde4b75cf67e0f3b
Phil Killewald <reallyasi9@users.noreply.github.com>
2021-03-09 16:47:48 -0500

    Add differential game spacing per column

--- src/app/tournament/tournament.component.ts
+++ src/app/tournament/tournament.component.ts
@@ -9,21 +9,25 @@ import { generateTournament } from '../mock-tournament';
 })
 export class TournamentComponent implements OnInit {
   tournament: Tournament = generateTournament(2021);
-  gameColumns: Iterable<number[]> = [
-    [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
-    [32,33,34,35,36,37,38,39],
-    [48,49,50,51],
-    [56,57],
-    [60,62,61],
-    [58,59],
-    [52,53,54,55],
-    [40,41,42,43,44,45,46,47],
-    [16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31],
+  gameColumns: Array<number[]> = [
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+    [32, 33, 34, 35, 36, 37, 38, 39],
+    [48, 49, 50, 51],
+    [56, 57],
+    [60, 62, 61],
+    [58, 59],
+    [52, 53, 54, 55],
+    [40, 41, 42, 43, 44, 45, 46, 47],
+    [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31],
   ];
 
   constructor() {}
 
   ngOnInit(): void {
     // this.tournament = generateTournament(2021);
+  }
+
+  columnToRound(col: number): number {
+    return Math.min(col, this.gameColumns.length - col - 1);
   }
 }
