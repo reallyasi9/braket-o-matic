7fbc28ccef7c54efbb224b343a1ac98e4d7094fc
Phil Killewald <reallyasi9@users.noreply.github.com>
2021-03-11 17:34:44 -0500

    Add pick propagation to pickem tournament

--- src/app/mock-tournament.ts
+++ src/app/mock-tournament.ts
@@ -1,16 +1,16 @@
 import { Game } from './game';
 import { generateGame } from './mock-games';
 import { generateTeam } from './mock-teams';
-import { Team } from './team';
 import { Tournament } from './tournament';
 
 // Generate a fake 64-team, 6-round, single-elimination tournament
 // that starts now and is not yet complete.
 export function generateTournament(id: string): Tournament {
-  var games: { [id: string]: Game } = {};
-  var cartilage: { [from: string]: { to: string; bottom: boolean } } = {};
-  var gridLocation: { [id: string]: { col: number; row: number } } = {};
+  const games: { [id: string]: Game } = {};
+  const cartilage: { [from: string]: { to: string; bottom: boolean } } = {};
+  const gridLocation: { [id: string]: { col: number; row: number } } = {};
   const gamesPerRound = [32, 16, 8, 4, 2, 1];
+  const cumulativeGamesPerRound = [32, 48, 56, 60, 62, 63];
   var iGame = 0;
   for (let round = 0; round < gamesPerRound.length; round++) {
     const nGames = gamesPerRound[round];
@@ -25,9 +25,11 @@ export function generateTournament(id: string): Tournament {
         games[iGame] = generateGame(iGame.toString());
       }
 
-      const nextGame = nGames + Math.floor(i / 2);
+      const nextGame = cumulativeGamesPerRound[round] + Math.floor(i / 2);
       const bottom = i % 2 == 1;
-      cartilage[iGame] = { to: nextGame.toString(), bottom: bottom };
+      if (iGame < cumulativeGamesPerRound[cumulativeGamesPerRound.length - 2]) {
+        cartilage[iGame] = { to: nextGame.toString(), bottom: bottom };
+      }
 
       const col = round >= 4 ? 4 : i >= nGames / 2 ? 8 - round : round;
       const rowOffset = round >= 4 ? 2 ** (round - 1) - 1 : 2 ** round - 1;
