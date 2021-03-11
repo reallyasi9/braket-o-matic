ddd904e20b72ccd4689fbc21994efeb9e4389c91
Phil Killewald <reallyasi9@users.noreply.github.com>
2021-03-11 16:57:48 -0500

    Make mock tournaments a service

--- src/app/mock-games.ts
+++ src/app/mock-games.ts
@@ -3,7 +3,7 @@ import { Team } from './team';
 
 const PERIODS: string[] = ['Pregame', '1st', '2nd', 'OT', 'OT2', 'Final'];
 
-export function generateGame(id: number, team1?: Team, team2?: Team): Game {
+export function generateGame(id: string, team1?: Team, team2?: Team): Game {
 
   const seconds = Math.floor(Math.random() * 60*20);
   const period = PERIODS[Math.floor(Math.random() * PERIODS.length)];
