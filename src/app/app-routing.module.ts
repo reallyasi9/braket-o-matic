b2fc5cc4a1c9b7e318176dda528c1cbf19a68991
Phil Killewald <reallyasi9@users.noreply.github.com>
2021-03-15 23:00:48 -0400

    Add guarded team-editor component with team service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
-import { UserLoginComponent } from './user-login/user-login.component';
 import { RouterModule, Routes } from '@angular/router';
 import { environment } from 'src/environments/environment';
 import { TournamentComponent } from './tournament/tournament.component';
 import { PickemTournamentComponent } from './pickem-tournament/pickem-tournament.component';
 import { LoggedInGuard } from 'ngx-auth-firebaseui';
+import { TeamEditorComponent } from './team-editor/team-editor.component';
+import { canActivate, hasCustomClaim } from '@angular/fire/auth-guard';
 
 export const TOURNAMENT_PATH: string = 'tournament';
 export const PICKEM_PATH: string = 'pickem';
+export const TEAM_EDIT_PATH: string = 'teamedit';
+
+const adminOnly = () => hasCustomClaim('admin');
 
 const routes: Routes = [
   { path: '', redirectTo: 'tournament', pathMatch: 'full' },
   { path: TOURNAMENT_PATH, component: TournamentComponent },
   { path: PICKEM_PATH, component: PickemTournamentComponent, canActivate: [LoggedInGuard] },
+  { path: TEAM_EDIT_PATH, component: TeamEditorComponent, ...canActivate(adminOnly) },
 ];
 
 @NgModule({
