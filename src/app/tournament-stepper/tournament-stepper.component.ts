55b7d5e5b5dabb7abf94caa8ba30b5033b9cfea4
Phil Killewald <reallyasi9@users.noreply.github.com>
2021-05-14 16:26:36 -0400

    Add game list manipulation functions

--- src/app/tournament-stepper/tournament-stepper.component.ts
+++ src/app/tournament-stepper/tournament-stepper.component.ts
@@ -57,17 +57,6 @@ export class TournamentStepperComponent implements OnInit {
     this.games = [game];
   }
 
-  addRound(): void {
-    const value = 2 ** this.tournament.roundValues.length;
-    this.tournament.roundValues.push(value);
-  }
-
-  deleteRound(): void {
-    if (this.tournament.roundValues.length > 1) {
-      this.tournament.roundValues.pop();
-    }
-  }
-
   addTeam(): void {
     const team = generateTeam();
     this.teams.push(team);
@@ -82,6 +71,37 @@ export class TournamentStepperComponent implements OnInit {
     this.teams = this.teams.filter((t) => t !== team);
     this.teamsRemaining = this.teamsRemaining.filter((t) => t !== team);
   }
+
+  addGame(from: Game, bottom: boolean): void {
+    if (this.noMoreGames()) {
+      this._snackBar.open('Add more teams before adding a play-in game.', 'Close');
+      return;
+    }
+    var insertIndex = this.games.indexOf(from);
+    if (insertIndex < 0) {
+      this._snackBar.open('Error finding play-in target.', 'Close')
+      return;
+    }
+    const game: Game = {
+      topTeam: undefined,
+      bottomTeam: undefined,
+      topPlayInGame: undefined,
+      bottomPlayInGame: undefined,
+    };
+    if (bottom) {
+      from.bottomPlayInGame = game;
+    } else {
+      from.topPlayInGame = game;
+    }
+    if (!bottom) {
+      insertIndex -= 1;
+    }
+    this.games.splice(insertIndex, 0, game);
+  }
+
+  // selectTeam(team: Team): void {
+  //   this.teamsRemaining = this.teamsRemaining.filter((t) => t !== team);
+  // }
 
   deleteGame(game: Game): void {
     this.games = this.games.filter((g) => g !== game);
